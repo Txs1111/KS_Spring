ce790270cd0ba09b73006c7fab3a0fa0f7c08e8a
Txs1111 <1831069264@qq.com>
2021-04-15 20:35:32 +0800

    aop面向切面编程

---
+++ KS_Spring/spring-09-aop/src/test/java/MyTest.java
@@ -0,0 +1,17 @@
+import com.T_Lyon.service.UserService;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MyTest {
+    @Test
+    public void test1() {
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        // 动态代理代理的是接口! 注意点
+        UserService userService = (UserService) context.getBean("userService");
+
+        userService.add();
+    }
+
+}
